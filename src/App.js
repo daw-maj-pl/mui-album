0d691193d3471b2b2d027e407426efb13ce05215
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-19 16:27:14 +0200

    Add cards and footer

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import PhotoCamera from '@material-ui/icons/PhotoCamera';
 
 import useStyles from './styles';
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const App = () => {
   const classes = useStyles();
 
@@ -63,7 +65,48 @@ const App = () => {
             </div>
           </Container>
         </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Aut temporibus possimus pariatur veniam! Non ipsa id ad
+                      dolorem, aliquid ab.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, ipsa!
+        </Typography>
+      </footer>
     </>
   );
 };
