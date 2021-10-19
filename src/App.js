bef713fcc760807cc4664e643f316225c8532acb
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-19 12:36:02 +0200

    Create App and import some MUI components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Container from '@material-ui/core/Container';
+import PhotoCamera from '@material-ui/icons';
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
