a899aa8940cab57615f7ad43ef6ffba4ec30b6db
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-19 14:16:33 +0200

    Add main section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
 
 const App = () => {
@@ -20,6 +21,45 @@ const App = () => {
           <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
       </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
+              doloribus repudiandae, nam a illum reiciendis. Quod deleniti
+              nesciunt eos architecto nisi consequuntur, numquam suscipit
+              facilis mollitia id? Necessitatibus, natus?
+            </Typography>
+            <div>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
     </>
   );
 };
